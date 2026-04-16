b03d2755d943bc9f9969c4fe7ef40f6527432f31
unknown <soumyadityadebnath997@gmail.com>
2026-04-17 02:09:24 +0530

    fix: add missing data-connect-wallet attribute to Navbar button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -136,6 +136,7 @@ export default function Navbar({ walletAddress, setWalletAddress, connectedWalle
             ) : (
               <motion.button
                 key="disconnected"
+                data-connect-wallet="true"
                 initial={{ opacity: 0, scale: 0.9 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.9 }}
